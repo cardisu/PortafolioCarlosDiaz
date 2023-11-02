ff8fd553d5460c16ce1098c98e162a5074774c7a
carlos diaz <70985820+cardisu@users.noreply.github.com>
2023-11-02 15:37:35 -0500

    Add files via upload

---
+++ pruebaTecnica/Modelo/cargo.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Numerics;
+
+namespace Modelo
+{
+    public class Cargo
+    {
+        public int Id { get; set; }
+        public string? Codigo { get; set; }
+
+        public string? Nombre { get; set; }
+        public bool? Activo { get; set; }
+
+        public Users? IdUsuarioCreacion { get; set; }
+
+        public string? Transaccion { get; set; }
+
+    }
+}
---
+++ pruebaTecnica/Modelo/users.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Modelo
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string? Usuario { get; set; }
+
+        public string? PrimerNombre { get; set; }
+
+        public string? SegundoNombre { get; set; }
+
+        public string? PrimerApellido { get; set; }
+
+        public string? SegundoApellido { get; set; }
+
+        public Departamento? IdDepartamento { get; set; }
+
+        public Cargo? IdCargo { get; set; }
+
+        public string? Transaccion { get; set; }
+
+    }
+}
